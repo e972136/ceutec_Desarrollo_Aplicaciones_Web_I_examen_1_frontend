c485f43d16b5afa2fb2279e81adbff6b55337734
Gaspar <e972136@unitec.edu>
2024-02-14 17:17:50 -0600

    Configuracion comportamiento paginas

--- src/components/Estimaciones.jsx
+++ src/components/Estimaciones.jsx
@@ -1,41 +1,90 @@
-import React from 'react'
+import React from "react";
 
 export const Estimaciones = () => {
+
+  const listado = [
+    {
+      id: 1,
+      placa: "4ab",
+      asegurado: "Juan perez",
+      estimadoPor: "Rosbinda",
+      fechaEvaluacion: "2022/10/14"
+    }
+    , {
+      id: 2,
+      placa: "32ss",
+      asegurado: "Mamerto Rodrigez",
+      estimadoPor: "Rosbinda",
+      fechaEvaluacion: "2022/10/14"
+    }
+    , {
+      id: 3,
+      placa: "21as",
+      asegurado: "Fendando Del Rincon",
+      estimadoPor: "Claudia",
+      fechaEvaluacion: "2022/10/14"
+    }
+  ];
+
+
   return (
     <div className="container">
       <h1>Estimaciones</h1>
-      <table className="table">
+      <form action="" method="get" autocomplete="off">
+        <div className="input-group mb-3">
+          <button className="btn btn-primary" type="submit" id="button-addon1">
+            <i className="fa fa-search"></i>
+          </button>
+          <input
+            type="text"
+            name="busqueda"
+            claclassNamess="form-control"
+            placeholder="Buscar"
+          />
+        </div>
+      </form>
+
+      <a  href="/crear" className="btn btn-success ">
+        Agregar Estimacion <i className="fa fa-plus-square" aria-hidden="true"></i>
+      </a>
+
+      <table className="table table-dark table-striped">
         <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Estimacion</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+          <tr className="bg-dark text-white">
+            <th className="col-1">Est</th>
+            <th className="col-1">Placa</th>
+            <th className="col-3">Asegurado</th>
+            <th className="col-3">Estimado por</th>
+            <th className="col-2">Fecha</th>
+            <th className="col-2">Acciones</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+
+
+          {
+            listado.map( (item) => (
+              <tr>
+                <td className="col-1" >{item.id}</td>
+                <td className="col-1" >{item.placa}</td>
+                <td className="col-3" >{item.asegurado}</td>
+                <td className="col-3" >{item.estimadoPor}</td>
+                <td className="col-2" >{item.fechaEvaluacion}</td>
+                <td className="col-2">
+                  <a className="btn btn-warning">Editar
+                    <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+                  </a>
+                </td>
+              </tr>
+            ))
+          }
+
+
+
+
+
         </tbody>
       </table>
     </div>
-    
-
-
-  )
-}
+  );
+};
--- src/components/EstimacionesCrear.jsx
+++ src/components/EstimacionesCrear.jsx
@@ -2,27 +2,195 @@ import React from 'react'
 
 export const EstimacionesCrear = () => {
   return (
-    <div className='container'>
-      <h1>EstimacionesCrear</h1>
+    <div className="container">     
+      <a href="/" className="btn btn-primary">Regresar</a>
+    <div className="row">
+        <div className="col">
+            <div className="card">
+                <div className="card-header">
+                    <h3>
+                        HOJA DE ESTIMACION
+                    </h3>
+                </div>
+                <div className="card-body">
+                    <form  method="POST"  className="row">
+                        <div className="col-3">
+                            <label for="estimacion"><b>Estimacion</b></label>
+                            <input type="text" className="form-control" id="estimacion"  readonly/>
+                        </div>
+
+                        <div className="col-3">
+                            <label for="fechaEvaluacion"><b>Fecha Evaluacion</b></label>
+                            <input type="date" className="form-control" id="fechaEvaluacion" />
+                        </div>
+                        <div className="col-6">
+                            <label for="implementadoPor"><b>Implementado Por</b></label>
+                            <input type="text" className="form-control" id="implementadoPor" readonly/>
+                        </div>
+                        <div className="col-6">
+                            <label for="asegurado"><b>Asegurado</b></label>
+                            <input type="text" className="form-control" id="asegurado"  autocomplete="on"/>
+                        </div>
+                        <div className="col-6">
+                            <label for="aseguradora"><b>Aseguradora</b></label>
+                            <input type="text" className="form-control" id="aseguradora"  autocomplete="on"/>
+                        </div>
+                        <div className="col-6">
+                            <label for="estimadoPor"><b>Estimado Por</b></label>
+                            <input type="text" className="form-control" id="estimadoPor" />
+                        </div>
+                        <div className="col-12">
+                            <h2>
+                                INFORMACION DEL VEHICULO
+                            </h2>
+                        </div>
+
+                        <div className="col-3">
+                            <label for="placa"><b>Placa</b></label>
+                            <input type="text" className="form-control" id="placa" />
+                        </div>    
+
+                        <div className="col-8"></div>
+
+                        <div className="col-3">
+                            <label for="marca"><b>Marca</b></label>
+                            <input type="text" className="form-control" id="marca" />
+                        </div>
+
+                        <div className="col-4">
+                            <label for="modelo"><b>Modelo</b></label>
+                            <input type="text" className="form-control" id="modelo" />
+                        </div>
+
+                        <div className="col-1">
+                            <label for="anioVehiculo"><b>Año</b></label>
+                            <input type="text" className="form-control" id="anioVehiculo" />
+                        </div>
+
+                        <div className="col-2">
+                            <label for="colorVehiculo"><b>Color</b></label>
+                            <input type="text" className="form-control" id="colorVehiculo" />
+                        </div>
+
+                        <div className="col-2">
+                            <label for="kM"><b>kM</b></label>
+                            <input type="text" className="form-control" id="kM" />
+                        </div>
+
+                        <div className="col-6">
+                            <label for="vinOSerie"><b>Vin O Serie</b></label>
+                            <input type="text" className="form-control" id="vinOSerie" />
+                        </div>
+
+                        <div className="col-6"></div>
 
 
-      <form>
-        <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1"/>
-        </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-            <label className="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
+                        
+                        <div className="col-3 mt-1">
+                            <label for="totalRepuestos" >Total de costos de repuestos: </label>
+                        </div>
+                        <div className="col-3 mt-1">
+                            <input type="text" className="form-control" id="totalRepuestos" 
+                                disabled/>
+                        </div>
+                        <div className="col-6  mt-1">
+                            <a  className="btn btn-warning form-control">
+                                LISTA DE REPUESTOS NECESARIOS A REEMPLAZAR
+                                <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+                            </a>
+                        </div>
 
+                        <div className="col-3  mt-1">
+                            <label for="totalReparaciones">Total de costos de mano de obra: </label>
+                        </div>
+                        <div className="col-3  mt-1">
+                            <input type="text" className="form-control" id="totalReparaciones" disabled/>
+                        </div>
+                        <div className="col-6  mt-1">
+                            <a className="btn btn-warning form-control">
+                                LISTA DE REPARACIONES
+                                <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+                            </a>
+                        </div>
+
+
+                        <div className="col-3  mt-1">
+                            <label for="totalReparacionesAdicionales" >Total reparaciones adicionales: </label>
+                        </div>
+                        <div className="col-3  mt-1">
+                            <input type="text" className="form-control " id="totalReparacionesAdicionales"
+                                 disabled/>
+                        </div>
+                        <div className="col-6  mt-1 ">
+                            <a 
+                                className="btn btn-warning form-control">
+                                LISTA DE REPARACIONES ADICIONALES
+                                <i className="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                        </div>
+                        
+                        <div className="col-3  mt-1">
+                            <label for="totalCostosAdicionalesMateriales" >Total costos adicionales (materiales): </label>
+                        </div>
+                        <div className="col-3 mt-1">
+                            <input type="number" step="0.01" className="form-control" id="totalCostosAdicionalesMateriales"/>                        
+                        </div>
+
+                        <div className="col-12">
+                            <h2>
+                                TOTALES
+                            </h2>
+                        </div>
+                        
+
+                        <div className="col-3  mt-1">
+                            <label for="totalCostosManoObra">Total Costos Mano de Obra: </label>
+                        </div>
+                        <div className="col-3 mt-1">
+                            <input type="number" step="0.01" className="form-control" id="totalCostosManoObra"/>
+                        </div>
+
+
+                        <div className="col-3 mt-1">
+                            <label for="CostoManoDeObraGerente">Costo mano de obra gerente: </label>
+                        </div>
+                        <div className="col-3 mt-1">
+                            <input type="number" step="0.01" className="form-control" id="CostoManoDeObraGerente"/>
+                        </div>
+
+                        <div className="col-3 mt-1">
+                            <label for="totalRepuestosT">Total Repuestos: </label>
+                        </div>
+                        <div className="col-3 mt-1">
+                            <input type="number" step="0.01" className="form-control" id="totalRepuestosT"/>
+                        </div>
+
+                        <div className="col-6 mt-1">
+
+                        </div>
+
+                        <div className="col-3 mt-1">
+                            <label for="totalCostosManoObraT">Total Costos Mano de Obra: </label>
+                        </div>
+                        <div className="col-3 mt-1">
+                            <input type="number" step="0.01" className="form-control" id="totalCostosManoObraT"/>
+                        </div>
+
+                        <div className="col-3  mt-1">
+                            <label for="costoMaterialesGerente" >Costo MaterialesG: </label>
+                        </div>
+                        <div className="col-3 mt-1">
+                            <input type="number" step="0.01" className="form-control" id="costoMaterialesGerente"/>
+                        </div>
+
+                        <div className="col-12 mt-3">
+                            <button type="submit" className="btn btn-primary form-control btn-lg">SAVE</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
   )
 }
